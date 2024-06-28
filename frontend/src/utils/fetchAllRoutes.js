baa1c52da1375f3e2295bb9519e4600e3f921c5f
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-06-28 16:57:14 +0100

    docs: added documentation

--- frontend/src/utils/fetchAllRoutes.js
+++ frontend/src/utils/fetchAllRoutes.js
@@ -1,3 +1,17 @@
+/**
+ * 
+ * @async function must be awaited
+ * @returns {Promise<Array>} Returns an array of Route Objects
+ * @throws {Error} Throws an error object if the fetch is unsuccessful
+ * @example
+ * try {
+ *  const routesArray = fetchAllRoutes() // array of routes will be stored in variable
+ *  const parsedRoutes = JSON.parse(routesArray) // the result must be parsed
+ * } catch (error){
+ *  console.error(error)
+ * }
+ * 
+ */
 export default async function fetchAllRoutes() {
   try {
     const data = await fetch("http://localhost:8080/getAllRoutes")
