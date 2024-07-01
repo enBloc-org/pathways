8cb3a4c418bcff714336537def02dae8caaf32c6
PL1106 <pleon0419@gmail.com>
2024-07-01 15:23:44 +0100

    Change the prop to just routes
    relates #35

---
+++ frontend/src/components/Dropdown.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function Dropdown({ routes }) {
+  const [selectedRoute, setSelectedRoute] = useState("");
+  function handleChange(event) {
+    setSelectedRoute(event.target.value);
+  }
+  if (!routes || routes.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <select id="dropdown" value={selectedRoute} onChange={handleChange}>
+        <option value="">--Please choose an option--</option>
+        {routes.map((route) => (
+          <option key={route.routeId} value={route.name}>
+            {route.name} ({route.routeId})
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
