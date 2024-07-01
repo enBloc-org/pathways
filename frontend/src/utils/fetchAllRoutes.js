16bcc125ccc03bc7de7a589dc024357e3730f5d5
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-01 17:05:56 +0100

    refactor: calling server base url from env variable
    relates #47

--- frontend/src/utils/fetchAllRoutes.js
+++ frontend/src/utils/fetchAllRoutes.js
@@ -1,5 +1,7 @@
+import "dotenv/config"
+
 /**
- * 
+ *
  * @async function must be awaited
  * @returns {Promise<Array>} Returns an array of Route Objects
  * @throws {Error} Throws an error object if the fetch is unsuccessful
@@ -10,16 +12,18 @@
  * } catch (error){
  *  console.error(error)
  * }
- * 
+ *
  */
 export default async function fetchAllRoutes() {
-  try {
-    const data = await fetch("http://localhost:8080/getAllRoutes")
-    const parsedData = await data.json()
+    try {
+        const data = await fetch(
+            `${process.env.SERVER}/getAllRoutes`
+        )
+        const parsedData = await data.json()
 
-    return parsedData
-  } catch (error) {
-    console.error(`Error fetching data: ${error}`)
-    throw error
-  }
+        return parsedData
+    } catch (error) {
+        console.error(`Error fetching data: ${error}`)
+        throw error
+    }
 }
