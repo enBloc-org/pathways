c4b5fee4d76afb615ffa65d1dae1ced4d932837d
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-01 17:00:43 +0100

    feat: util to controll fetching single route by Id

--- backend/server/routes/getRouteById.js
+++ backend/server/routes/getRouteById.js
@@ -1,7 +1,7 @@
 import "dotenv/config"
 
 export default async function getRouteById(givenId) {
-    if (typeof givenId !== "string")
+    if (typeof givenId !== "string" || !givenId)
         throw new Error("Id value not acceptable")
 
     const url = `${process.env.BASE_URL}/Routes/${givenId}`
@@ -9,17 +9,20 @@ export default async function getRouteById(givenId) {
     const options = {
         method: "GET",
         headers: {
-            "X-API-KEY": process.env.API_KEY,
             accept: "application/json",
+            "X-API-KEY": process.env.API_KEY,
         },
     }
 
-    try{
-      const response = await fetch(url, options)
-      const data = await response.json()
-      return data
-    }catch(error){
-      console.error(`Error fetching data: ${error}`)
-      throw error
+    try {
+        const response = await fetch(url, options)
+        if (!response.ok)
+            throw new Error("Error fetching data")
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(`Error fetching data: ${error}`)
+        throw error
     }
 }
