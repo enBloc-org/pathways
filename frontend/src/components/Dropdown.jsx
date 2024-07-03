23ab99ad6783d57c905ace3c535a646eec9377a9
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-03 16:00:18 +0100

    fix: corrected state handling

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 
 export default function Dropdown({ routes }) {
+    const [selectedRoute, setSelectedRoute] =
+        useState("")
     function handleChange(event) {
         setSelectedRoute(event.target.value)
     }
@@ -29,3 +31,4 @@ export default function Dropdown({ routes }) {
             </select>
         </div>
     )
+}
