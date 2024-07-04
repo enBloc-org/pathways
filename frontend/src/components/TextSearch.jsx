3d5183241b0e4df8578feec18a327b9caecaa29b
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-04 11:41:17 +0100

    feat: updating state with user input
    relates #57

--- frontend/src/components/TextSearch.jsx
+++ frontend/src/components/TextSearch.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react"
 
 export default function TextSearch(searchHandler) {
+    const [searchParameter, setSearchParameter] =
+        useState(undefined)
+
+    const inputHandler = event => {
+        setSearchParameter(event.target.value)
+    }
+
     return (
         <>
             <form>
                 <input
-                    value=""
-                    onChange={searchHandler}
+                    value={searchParameter}
+                    onChange={inputHandler}
                     placeholder="Enter your search parameter"
                 ></input>
                 <button type="button">search</button>
             </form>
+            {searchParameter && (
+                <p>You searched: {searchParameter}</p>
+            )}
         </>
     )
 }
