5a18a979a3bdfe4b7e61ff7a50b4b0b41c5519c0
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-05 14:45:57 +0100

    feat: added fetchAllRoutes util

---
+++ frontend/src/utils/fetchAllRoutes.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @async this function must be awaited
+ * @returns {Promise<Array>} Returns an array of Route Objects
+ * @throws {Error} Throws an error object if the fetch is unsuccessful
+ * @example
+ * try {
+ *  const routesArray = await fetchAllRoutes() // array of routes will be stored in variable
+ *  const parsedRoutes = await JSON.parse(routesArray) // the result must be parsed
+ * } catch (error){
+ *  console.error(error)
+ * }
+ *
+ */
+export default async function fetchAllRoutes() {
+    try {
+        const data = await fetch(
+            `${import.meta.env.SERVER}/getAllRoutes`
+        )
+        const parsedData = await data.json()
+
+        return parsedData
+    } catch (error) {
+        console.error(`Error fetching data: ${error}`)
+        throw error
+    }
+}
