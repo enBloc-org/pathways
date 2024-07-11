3b8557db0f2c9069dbb9a9ac8c2181ccf2a0deb0
PL1106 <pleon0419@gmail.com>
2024-07-11 15:43:14 +0100

    created Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import gatsbylogo from '../image/gatsbylogo.jpeg';
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={gatsbylogo} alt="Gatsby Logo" className="logo" />
+      </div>
+      <input type="text" placeholder="Search a pathway" className="search-bar" />
+    </header>
+  );
+};
+
+export default Header;
