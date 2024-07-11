85d2538fb3959510c174309f8943ad59c3abe895
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-11 18:06:57 +0100

    style: importing textsearch.css into the component
    relates #57

--- frontend/src/components/TextSearch.jsx
+++ frontend/src/components/TextSearch.jsx
@@ -2,27 +2,27 @@ import { useState } from "react"
 import "../style/TextSearch.css"
 
 export default function TextSearch(searchHandler) {
-    const [searchParameter, setSearchParameter] =
-        useState(undefined)
+  const [searchParameter, setSearchParameter] =
+    useState(undefined)
 
-    const inputHandler = event => {
-        setSearchParameter(event.target.value)
-    }
+  const inputHandler = event => {
+    setSearchParameter(event.target.value)
+  }
 
-    const submitHandler = () => {
-        searchHandler(searchParameter)
-    }
+  const submitHandler = () => {
+    searchHandler(searchParameter)
+  }
 
-    return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <input
-                    value={searchParameter}
-                    onChange={inputHandler}
-                    placeholder="Enter your search parameter"
-                />
-                <button type="button">search</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          value={searchParameter}
+          onChange={inputHandler}
+          placeholder="Enter your search parameter"
+        />
+        <button type="button">⏎</button>
+      </form>
+    </div>
+  )
 }
