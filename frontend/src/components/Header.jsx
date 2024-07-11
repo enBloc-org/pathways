7877d1d2e2e457e9b5cff36eab71fe744ed76e90
PL1106 <pleon0419@gmail.com>
2024-07-11 15:51:32 +0100

    add gatsby logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import gatsbylogo from '../image/gatsbylogo.jpeg';
+import gatsbyLogo from "../../public/GATSBY.jpeg"
 import "../styles/header.css";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src={gatsbylogo} alt="Gatsby Logo" className="logo" />
+        <img src={gatsbyLogo} alt="Gatsby Logo" className="logo" />
       </div>
       <input type="text" placeholder="Search a pathway" className="search-bar" />
     </header>
