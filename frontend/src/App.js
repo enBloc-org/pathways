b30a98608dc44ec1ce812532007eaad795697e1d
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-15 17:49:22 +0100

    feat: declared raised state for search results and handler function for the header component
    relates #87

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,22 @@
-import './App.css';
-import Dropdown from './components/Dropdown';
-import ClusterCard from './components/ClusterCard'
-import fetchAllRoutes from './utils/fetchAllRoutes';
-import fetchOccupationByQuery from './utils/fetchOccupationByQuery'
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
+import "./style/globals.css"
+import fetchOccupationByQuery from "./utils/fetchOccupationByQuery"
+import { useEffect, useState } from "react"
+import Header from "./components/Header"
 
 function App() {
-  const [allRoutes, setAllRoutes] = useState([])
-  const [selectedRoute, setSelectedRoute] = useState("")
-  const [routeDetails, setRouteDetails] = useState(null)
+  const [searchResults, setSearchResults] = useState(undefined)
 
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const fetchedRoutes = await fetchAllRoutes()
-        setAllRoutes(fetchedRoutes)
-      } catch (error) {
-        console.error("Failed to fetch routes:", error)
-      }
-    }
-    fetch()
-  }, [])
-
-  useEffect(() => {
-    if (selectedRoute) {
-      const route = allRoutes.find((route) => route.routeId === parseInt(selectedRoute, 10));
-      setRouteDetails(route);
-    }
-  }, [selectedRoute, allRoutes])
+  const handleSearch = async query => {
+    const data = await fetchOccupationByQuery(query)
+    setSearchResults(data)
+  }
 
   return (
     <div className="App">
-      <Header searchHandler={fetchOccupationByQuery} />
-      <h1>Route Details</h1>
-      <Dropdown
-        routes={allRoutes}
-        selectedRoute={selectedRoute}
-        setSelectedRoute={setSelectedRoute}
-        showId={false}
-      />
-      {routeDetails ? (
-          <ClusterCard
-            name={routeDetails.name}
-            description={`ID: ${routeDetails.routeId}`}
-            technicalLevelName={`Technical Level: ${routeDetails.technicalLevelName || 'N/A'}`}
-          />
-      ) : (
-        <div>Select a route to see details.</div>
-      )}
+      <Header searchHandler={handleSearch} />
+      {searchResults && <p>{searchResults.results[0].object}</p>}
     </div>
   )
 }
 
-export default App;
+export default App
