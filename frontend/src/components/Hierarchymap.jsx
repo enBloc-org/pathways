af154aa002b9085642ff1632cf464778a00615e8
PL1106 <pleon0419@gmail.com>
2024-07-18 12:29:16 +0100

    Made 3 cards and trying to make them work together into one grid
    relates #67

--- frontend/src/components/Hierarchymap.jsx
+++ frontend/src/components/Hierarchymap.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import OccupationDetails from './OccupationDetails';
-import '../style/HierarchyMap.css';
 
 const HierarchyMap = ({ occupation, routeName }) => {
   return (
     <div className="hierarchy-map">
       <h1 className="route-header">{routeName}</h1>
+      <OccupationDetails occupation={occupation} routeName={routeName} />
+      <OccupationDetails occupation={occupation} routeName={routeName} />
       <OccupationDetails occupation={occupation} routeName={routeName} />
     </div>
   );
