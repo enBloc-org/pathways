7f4c0154003f2dde4846336c9df8a923f7e9e812
PL1106 <pleon0419@gmail.com>
2024-07-18 16:38:17 +0100

    Made visual changes so it looks more alike the example given
    relates #67

--- frontend/src/components/Hierarchymap.jsx
+++ frontend/src/components/Hierarchymap.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import OccupationDetails from './OccupationDetails';
+import '../style/Hierarchymap.css'; 
 
-const HierarchyMap = ({ occupation, routeName }) => {
+const HierarchyMap = ({}) => {
   return (
-    <div className="hierarchy-map">
-      <h1 className="route-header">{routeName}</h1>
-      <OccupationDetails occupation={occupation} routeName={routeName} />
-      <OccupationDetails occupation={occupation} routeName={routeName} />
-      <OccupationDetails occupation={occupation} routeName={routeName} />
+    <div className="hierarchy-map-wrapper">
+      <p className="route-header">Digital</p>
+      <div className="hierarchy-map">
+        <div className="grid-container">
+          <div className="grid-item">Technical</div>
+          <div className="grid-line"></div>
+          <div className="grid-item">Higher Technical</div>
+          <div className="grid-line"></div>
+          <div className="grid-item">Professional</div>
+        </div>
+      </div>
     </div>
   );
 };
