158262baf3e168b67b6bc69d8358576e4f248bac
PL1106 <pleon0419@gmail.com>
2024-07-22 15:24:50 +0100

    refactor: removed unused argument
    relates #67

--- frontend/src/components/Hierarchymap.stories.js
+++ frontend/src/components/Hierarchymap.stories.js
@@ -6,21 +6,19 @@ export default {
   component: HierarchyMap,
 };
 
-const exampleOccupation = {
-  name: 'Software Developer',
-  level: 2,
-  mapHierarchy: {
-    technicalLevelName: 'Intermediate',
-    pathwayName: 'Development',
-  },
-  overview: 'Develops and maintains software applications.',
-  summary: '<p>In-depth details about the role of a software developer...</p>',
-};
-
 const Template = (args) => <HierarchyMap {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  occupation: exampleOccupation,
   routeName: 'Digital',
+  exampleOccupation: {
+    name: 'Software Developer',
+    level: 2,
+    mapHierarchy: {
+      technicalLevelName: 'Intermediate',
+      pathwayName: 'Development',
+    },
+    overview: 'Develops and maintains software applications.',
+    summary: '<p>In-depth details about the role of a software developer...</p>',
+  },
 };
