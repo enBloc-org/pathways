6301fa5a500584be5f8d251d6e9124e104a8400e
PL1106 <pleon0419@gmail.com>
2024-07-22 14:38:54 +0100

    Increased size lettering
    relates #67

--- frontend/src/components/Hierarchymap.jsx
+++ frontend/src/components/Hierarchymap.jsx
@@ -7,9 +7,15 @@ const HierarchyMap = () => {
       <p className="route-header">Digital</p>
       <div className="hierarchy-map">
         <div className="heading">
-            <p className="heading-item">Technical</p>            
-            <p className="heading-item">Higher Technical</p>     
-            <p className="heading-item">Professional</p>        
+          <div className="heading-item">
+            <p>Technical</p>
+          </div>
+          <div className="heading-item">
+            <p>Higher Technical</p>
+          </div>
+          <div className="heading-item">
+            <p>Professional</p>
+          </div>
         </div>
         <div className="grid-container">
           <div className="grid-line line-1"></div>
