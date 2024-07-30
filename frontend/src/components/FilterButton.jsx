aad0a4649136f70971246c258853a41ea68e5df1
PL1106 <pleon0419@gmail.com>
2024-07-30 15:16:35 +0100

    Made adjustments to the css
    relates #113

--- frontend/src/components/FilterButton.jsx
+++ frontend/src/components/FilterButton.jsx
@@ -26,7 +26,7 @@ export default function FilterButton({ options, onApply }) {
   return (
     <div className="filter-container">
       <button className="filter-toggle-button" onClick={toggleDropdown}>
-        Filter Result
+        <span className="filter-results">Filter Result</span>
         <img src={filterIcon} alt="Filter Icon"/>
       </button>
       {isDropdownOpen && (
