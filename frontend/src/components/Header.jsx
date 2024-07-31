da5d40269f479e23f50b7e5807631e18cd2e7e78
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-31 13:08:18 +0100

    refactor: make Header into a nav component for ease of layout setup to the app
    relates #117

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,24 @@
+import { Link } from "react-router-dom"
+
 import TextSearch from "./TextSearch"
 import gatsbyLogo from "../images/GATSBY.jpeg"
 import "../style/Header.css"
 
 export default function Header({ searchHandler }) {
   return (
-    <header className="header">
+    <nav className="header">
       <div className="logo-container">
         <img src={gatsbyLogo} alt="Gatsby Logo" className="logo" />
       </div>
       <TextSearch searchHandler={searchHandler} />
       <div className="button-container">
-        <a className="header-button">About</a>
-        <a className="header-button" type="button">
+        <Link className="header-button" to="/about">
+          About
+        </Link>
+        <Link className="header-button" to="/search">
           Search
-        </a>
+        </Link>
       </div>
-    </header>
+    </nav>
   )
 }
