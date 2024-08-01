bc158af7979f4d0f4e96221552cd12760943aae9
PL1106 <pleon0419@gmail.com>
2024-08-01 16:18:12 +0100

    Changed the app.js file
    relates #112

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,40 @@
-import React, { useEffect, useState } from "react";
-import fetchOccupationByQuery from "./utils/fetchOccupationByQuery";
-import fetchAllRoutes from "./utils/fetchAllRoutes.js";
-import Header from "./components/Header";
-import OccupationsList from "./components/OccupationsList";
-import FilterContainer from "./components/FilterContainer.jsx";
-import "./style/globals.css";
-import "./App.css";
+import { useState } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+
+import fetchOccupationByQuery from "./utils/fetchOccupationByQuery"
+import Header from "./components/Header"
+import About from "./pages/About"
+import Search from "./pages/Search"
+import OccupationPage from "./pages/OccupationPage"
+
+import "./style/globals.css"
+import "./App.css"
 
 function App() {
-  const [searchResults, setSearchResults] = useState(undefined);
-  const [allRoutes, setAllRoutes] = useState([]);
-  const [appliedFilters, setAppliedFilters] = useState([]);
-  const [occupationSearch, setOccupationSearch] = useState('');
+  const [searchResults, setSearchResults] = useState(undefined)
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const fetchRoutes = async () => {
-      try {
-        const fetchedRoutes = await fetchAllRoutes();
-        setAllRoutes(fetchedRoutes);
-      } catch (error) {
-        console.error("Failed to fetch routes:", error);
-      }
-    };
-    fetchRoutes();
-  }, []);
-
-  const handleSearch = async (query) => {
-    const data = await fetchOccupationByQuery(query);
-    setOccupationSearch(query);
-    setSearchResults(data);
-    window.location.hash = query;
-  };
+  const handleSearch = async query => {
+    const data = await fetchOccupationByQuery(query)
+    setSearchResults(data.results)
+    navigate("/search")
+  }
 
   return (
     <div className="app">
-      <Header searchHandler={handleSearch} allRoutes={allRoutes} />
-      <FilterContainer 
-        allRoutes={allRoutes} 
-        appliedFilters={appliedFilters}
-        setAppliedFilters={setAppliedFilters}
-        occupationSearch={occupationSearch}
-      />
-      {searchResults && (
-        <div style={{ width: "100dvw" }}>
-          <OccupationsList occupationsArray={searchResults.results} />
-          <div style={{ width: "70vmin" }}></div>
-        </div>
-      )}
+      <Header searchHandler={handleSearch} />
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/search"
+          element={<Search searchResults={searchResults} />}
+        />
+        <Route
+          path="/occupation-details/:occupation"
+          element={<OccupationPage />}
+        />
+      </Routes>
     </div>
   );
 }
