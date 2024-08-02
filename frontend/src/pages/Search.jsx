3b196b2bcf39f1c11b045a5a49ef548307bb11f0
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-02 15:21:43 +0100

    refactor: displaying occupations list with tlevels matching the user search in search page
    relates #125/filter-occupations

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,12 +1,16 @@
-export default function Search( {searchResults} ) {
+import OccupationsList from "../components/OccupationsList"
+
+export default function Search({ searchResults }) {
   return (
     <>
       <h1>Search Page</h1>
-      <ul>
-        {searchResults && searchResults.map(result => (
-          <li>{result.name}</li>
-        ))}
-      </ul>
+      {searchResults.length === 0 ? (
+        <p>
+          There are no T Level placements that match this job title
+        </p>
+      ) : (
+        <OccupationsList occupationsArray={searchResults} />
+      )}
     </>
   )
 }
