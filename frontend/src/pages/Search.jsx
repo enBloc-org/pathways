1852b9c0c69301fe76b60a1ee3c78fd17ade1987
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-05 18:11:42 +0100

    refactor: re-arranged rendering options into a switch statement declared outside of the return block for better readability
    relates #130

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,24 +1,20 @@
 import OccupationsList from "../components/OccupationsList"
-
 export default function Search({ searchResults, searchStatus }) {
-  switch(searchStatus){
-    case 'idle':
-      return <p>Enter search terms</p>
-    case 'loading':
-      return <p>Loading...</p>
-    case 'fulfilled':
-      return <OccupationsList occupationsArray={searchResults} />
+  const renderStatusResults = () => {
+    switch (searchStatus) {
+      case "idle":
+        return <p>Enter search terms</p>
+      case "loading":
+        return <p>Loading...</p>
+      case "fulfilled":
+        return <OccupationsList occupationsArray={searchResults} />
+    }
   }
-  // return (
-  //   <>
-  //     <h1>Search Page</h1>
-  //     {isLoading ? (
-  //       <p>Loading...</p>
-  //     ) : (
-  //       searchResults && (
-  //         <OccupationsList occupationsArray={searchResults} />
-  //       )
-  //     )}
-  //   </>
-  // )
+
+  return (
+    <>
+      <h1>Search Page</h1>
+      {renderStatusResults()}
+    </>
+  )
 }
