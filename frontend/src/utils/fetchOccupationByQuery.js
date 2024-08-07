bf1a7fd0aace7af429d47e4c4351fd341926ffba
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-07 17:20:03 +0100

    fix: correct return value to current data structure
    relates #134

--- frontend/src/utils/fetchOccupationByQuery.js
+++ frontend/src/utils/fetchOccupationByQuery.js
@@ -28,7 +28,7 @@ export default async function fetchOccupationByQuery(givenQuery) {
       )
       const parsedData = await data.json()
 
-      result.push(...parsedData)
+      result.push(...parsedData.results)
     } catch (error) {
       console.error(`Error fetching data: ${error}`)
       throw error
