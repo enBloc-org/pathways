261f3536677380679d5550317d50c005ff7e5173
Pedro Leon <145766670+PL1106@users.noreply.github.com>
2024-08-07 15:49:44 +0100

    I added the filter button to the search.jsx file
    relates #99

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,12 +1,42 @@
-export default function Search( {searchResults} ) {
+import React, { useState } from "react";
+
+import FilterButton from '../components/FilterButton'
+import fetchAllRoutes from '../utils/fetchAllRoutes'
+
+export default function Search({ searchResults }) {
+  const [filteredResults, setFilteredResults] = useState(searchResults);
+  const [allRoutes, setAllRoutes] = useState()
+
+  const handleApplyFilters = (selectedOptions) => {
+    if (selectedOptions.length === 0) {
+      setFilteredResults(searchResults);
+    } else {
+      setFilteredResults(searchResults.filter(result =>
+        selectedOptions.includes(result.routeId)
+      ));
+    }
+  };
+
+  useEffect(()=>{
+    const fetchOptions = async () => {
+      const filterOptions = await fetchAllRoutes()
+      setAllRoutes(filterOptions)
+    }
+
+    fetchOptions()
+  }, [])
+
   return (
     <>
       <h1>Search Page</h1>
+      <FilterButton options={allRoutes} onApply={handleApplyFilters} />
+      
       <ul>
-        {searchResults && searchResults.map(result => (
-          <li>{result.name}</li>
+        {filteredResults && filteredResults.map(result => (
+          <li key={result.routeId}>{result.name}</li>
         ))}
       </ul>
     </>
-  )
+  );
 }
+
