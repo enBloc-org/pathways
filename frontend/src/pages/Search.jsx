b871543b0085185725b117b72660a9f40fee8fb6
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-07 12:20:02 +0100

    refactor: moved message for no results our of search page
    relates #125

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -4,13 +4,7 @@ export default function Search({ searchResults }) {
   return (
     <>
       <h1>Search Page</h1>
-      {searchResults.length === 0 ? (
-        <p>
-          There are no T Level placements that match this job title
-        </p>
-      ) : (
-        <OccupationsList occupationsArray={searchResults} />
-      )}
+      <OccupationsList occupationsArray={searchResults} />
     </>
   )
 }
