bc3f13adb7394ed013c469d7bc922a0dbc758e3c
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-07 18:10:54 +0100

    refactor: drilled saved status from page
    relates #134

--- frontend/src/components/SaveSearchButton.jsx
+++ frontend/src/components/SaveSearchButton.jsx
@@ -3,11 +3,11 @@ import heartIcon from '../images/heart.svg';
 import heartRedIcon from '../images/redheart.svg';
 import "../style/Heart.css";
 
-export default function SaveSearchButton({ onSave, onUnsave }) {
+export default function SaveSearchButton({ onSave, onUnsave, isSaved }) {
   const [isHeartClicked, setIsHeartClicked] = useState(false);
 
   const toggleHeart = () => {
-    if (isHeartClicked) {
+    if (isSaved) {
       onUnsave();
     } else {
       onSave();
@@ -19,7 +19,7 @@ export default function SaveSearchButton({ onSave, onUnsave }) {
     <button className="save-search-button" onClick={toggleHeart}>
       <span className="save-search-text">Save search</span>
       <img
-        src={isHeartClicked ? heartRedIcon : heartIcon} 
+        src={isSaved ? heartRedIcon : heartIcon} 
         alt="Heart Icon"
         className="heart-icon"
       />
