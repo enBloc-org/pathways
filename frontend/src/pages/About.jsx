c7e677f3fdc0944a757f0a4a0c5d7f740af70a5e
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-13 09:34:23 +0100

    feat: set '/about' route
    relates #129

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,15 @@
+import youngProfessionals from "../images/young-professionals.jpg"
+
 export default function About() {
-  return <h1>About Page</h1>
+  return (
+    <>
+      <h1>About Page</h1>
+      <div>
+        <img className="about-page__image" src={youngProfessionals} />
+        <p>
+
+        </p>
+      </div>
+    </>
+  )
 }
