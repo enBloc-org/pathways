0b682fe463c1a02aeddd115b39f8e095c74ffecf
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-14 12:32:29 +0100

    chore: set up SearchPage fixture
    relates #89

---
+++ frontend/cypress/support/page_objects/SearchPage.js
@@ -0,0 +1,19 @@
+import PageLayout from "./PageLayout"
+
+class SearchPage extends PageLayout {
+  saveButton() {
+    return cy
+      .get('button[class="save-search-button"]')
+      .contains(/(remove search)|(save search)/i)
+      .should("be.visible")
+  }
+
+  heartIcon() {
+    return cy
+      .get('svg[class="heart-icon"]')
+      .contains(/(remove search)|(save search)/i)
+      .should("be.visible")
+  }
+}
+
+export default new SearchPage()
