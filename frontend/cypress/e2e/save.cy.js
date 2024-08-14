8ffcfd2286d1f61101805988b0fae7a3ec677a45
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-14 13:16:36 +0100

    test: testing memory permanence for saving feature
    relates #89

--- frontend/cypress/e2e/save.cy.js
+++ frontend/cypress/e2e/save.cy.js
@@ -1,19 +1,37 @@
 import * as page from "../fixtures/URLs.json"
+import * as input from "../fixtures/inputs.json"
 import SearchPage from "../support/page_objects/SearchPage"
 
 describe("Save search feature", () => {
-  beforeEach(() => {
+  before(() => {
     cy.clearLocalStorage()
+  })
+
+  beforeEach(() => {
+    cy.restoreLocalStorage()
     cy.visit(page.search)
-    SearchPage.searchBar().type("web developer")
+    SearchPage.searchBar().type(input.searchSingle)
     SearchPage.searchButton().click()
   })
 
+  afterEach(() => {
+    cy.saveLocalStorage()
+  })
+
   it("allows user to save any search", () => {
+    SearchPage.saveButton().contains(/save search/i)
     SearchPage.saveButton().click()
     SearchPage.saveButton().contains(/remove search/i)
     SearchPage.heartIcon()
       .invoke("attr", "fill")
-      .then(colour => expect(colour).to.match(/#ff0000/))
+      .then(color => expect(color).to.match(/#ff0000/))
+  })
+
+  it("remembers a search that was previously saved", () => {
+    SearchPage.heartIcon()
+      .invoke("attr", "fill")
+      .then(color => expect(color).to.match(/#ff0000/))
+    SearchPage.searchBar().clear().type(input.searchMultiple)
+    SearchPage.searchButton().click()
   })
 })
