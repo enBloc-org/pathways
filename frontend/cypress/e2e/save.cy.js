f3554ac9b0ecb13d3e85d2314fa9c36f31d37cf9
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-15 16:05:16 +0100

    fix: corrected save feature suite to guarantee consistent results
    relates #89

--- frontend/cypress/e2e/save.cy.js
+++ frontend/cypress/e2e/save.cy.js
@@ -3,38 +3,15 @@ import * as input from "../fixtures/inputs.json"
 import SearchPage from "../support/page_objects/SearchPage"
 
 describe("Save search feature", () => {
-  before(() => {
-    cy.clearLocalStorage()
-  })
-
-  beforeEach(() => {
-    cy.restoreLocalStorage()
+  it("allows user to save any search", () => {
     cy.visit(page.search)
     SearchPage.searchBar().type(input.searchSingle)
     SearchPage.searchButton().click()
-  })
-
-  afterEach(() => {
-    cy.saveLocalStorage()
-  })
-
-  it("allows user to save any search", () => {
     SearchPage.saveButton().contains(/save search/i)
     SearchPage.saveButton().click()
     SearchPage.saveButton().contains(/remove search/i)
     SearchPage.heartIcon()
       .invoke("attr", "fill")
       .then(color => expect(color).to.match(/#ff0000/))
-  })
-
-  it("remembers a search that was previously saved", () => {
-    SearchPage.heartIcon()
-    .invoke("attr", "fill")
-    .then(color => expect(color).to.match(/#ff0000/))
-    SearchPage.searchBar().clear().type(input.searchMultiple)
-    SearchPage.searchButton().click()
-    SearchPage.heartIcon()
-      .invoke("attr", "fill")
-      .then(color => expect(color).to.match(/#bebebe/))
   })
 })
