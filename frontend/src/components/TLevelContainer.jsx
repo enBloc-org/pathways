109b422795a3c6b751609fb6ed2da08d83ca871b
Nich Galzin <nichgalzin@gmail.com>
2024-08-19 08:10:42 +0100

    test: storybook for t-level container
    Relates #123

--- frontend/src/components/TLevelContainer.jsx
+++ frontend/src/components/TLevelContainer.jsx
@@ -7,19 +7,18 @@ export default function TLevelContainer({ products }) {
       <h2>T-Level Placements</h2>
       {products.map((product, index) => {
         if (product.typeName === "TLevel") {
-          return (
-            <TLevelCard
-              key={index}
-              name={product.name}
-              productCode={product.productCode}
-            />
-          )
+          return <TLevelCard key={index} name={product.name} />
         }
+        return null
       })}
       <div className="t-level-container__links">
         <p>Learn More:</p>
-        <a href="https://employers.tlevels.gov.uk/hc/en-gb">T-level Placements</a>
-        <a href="https://www.tlevels.gov.uk/students/find">Find Partners</a>
+        <a href="https://employers.tlevels.gov.uk/hc/en-gb">
+          T-level Placements
+        </a>
+        <a href="https://www.tlevels.gov.uk/students/find">
+          Find Partners
+        </a>
       </div>
     </aside>
   )
