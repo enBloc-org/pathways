8658ba010894346102887a6194ddda81be21998c
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-21 16:24:59 +0100

    build: setup base url as env variable for ci in cypress
    relates #143

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.FRONTEND,
     viewportWidth: 1920,
     viewportHeight: 1080,
   },
