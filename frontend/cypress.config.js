a95f06f24e8d950c915df13e983e5ab965805f1b
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-21 16:41:35 +0100

    ci: revert cypress base url set up
    relates #143

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: process.env.FRONTEND,
+    baseUrl: "http://localhost:3000",
     viewportWidth: 1920,
     viewportHeight: 1080,
   },
