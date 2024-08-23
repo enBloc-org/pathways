2c07d6eeeccebe0461c70bf6002bbc5657bf7083
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-23 11:08:35 +0100

    style: correct about page copy and layout
    relates #153

--- frontend/src/components/InfoPage.jsx
+++ frontend/src/components/InfoPage.jsx
@@ -2,20 +2,17 @@ import "../style/InfoPage.css"
 import backgroundImage from "../images/HomeBackground.jpg"
 
 function InfoPage() {
-  
-
   return (
-    <main className="info-page main" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <main
+      className="info-page main"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="info-page__wrapper">
-        <h1 className='info-page__text'> 
-          T-Level stuff
-        </h1>
-        <p className="info-page__text">
-          All In One Place
-        </p>
+        <h1 className="info-page__text">Pathways</h1>
+        <p className="info-page__text">Explore T-Level placements for team</p>
       </div>
     </main>
-  );
+  )
 }
 
-export default InfoPage;
+export default InfoPage
