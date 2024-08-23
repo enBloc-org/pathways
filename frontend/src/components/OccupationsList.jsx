fb81ebc4e641700baf6d22e92bbcc2260ba8e0d1
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-23 09:49:52 +0100

    style: change occupations list display to grid
    relates #159

--- frontend/src/components/OccupationsList.jsx
+++ frontend/src/components/OccupationsList.jsx
@@ -3,9 +3,9 @@ import "../style/OccupationsList.css"
 
 export default function OccupationsList({ occupationsArray }) {
   return (
-    <div className="container">
+    <div className="occupations-list--container">
       {occupationsArray.length === 0 ? (
-        <p>There are no occupations matching your search terms</p>
+        <p className="occupations-list__empty">There are no occupations matching your search terms</p>
       ) : (
         occupationsArray.map(occupation => (
           <OccupationCard
