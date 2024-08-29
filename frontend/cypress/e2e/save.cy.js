f3f14281497a4c1e9ca5e90db19d3bb51afc5341
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-29 09:22:06 +0100

    test: extend testing to check that save button is disabled if no query is set
    relates #162

--- frontend/cypress/e2e/save.cy.js
+++ frontend/cypress/e2e/save.cy.js
@@ -7,6 +7,11 @@ describe("Save search feature", () => {
     cy.clearAllLocalStorage()
   })
 
+  it("stops the user from saving a search without a valid query", () => {
+    cy.visit(page.search)
+    SearchPage.saveButton().should("be.disabled")
+  })
+
   it("allows user to save any search", () => {
     cy.visit(page.search)
     SearchPage.searchBar().type(input.searchSingle)
@@ -25,32 +30,33 @@ describe("Save search feature", () => {
 describe("visit saved search button", () => {
   it("allows users to change the search results to something they saved", () => {
     cy.visit(page.search)
-      
+
     SearchPage.searchBar().type(input.searchSingle)
     SearchPage.searchButton().click()
-    cy.get("div").contains(/software development technician/i).should("exist").and("be.visible")
+    cy.get("div")
+      .contains(/software development technician/i)
+      .should("exist")
+      .and("be.visible")
     SearchPage.saveButton().click()
-    SearchPage.savedSearchButton().contains(/Saved Searches/i)   
+    SearchPage.savedSearchButton().contains(/Saved Searches/i)
     SearchPage.searchBar().type(input.searchSingleAlt)
     SearchPage.searchButton().click()
 
+    SearchPage.savedSearchButton().click()
 
-    SearchPage.savedSearchButton().click() 
-    
-    
-    cy.get('.dropdown-menu').should('be.visible')
+    cy.get(".dropdown-menu").should("be.visible")
 
-
-    cy.get('.dropdown-item')
-      .contains('web developer filters(0)')
-      .should('exist')
-      .should('be.visible')
+    cy.get(".dropdown-item")
+      .contains("web developer filters(0)")
+      .should("exist")
+      .should("be.visible")
       .click()
-    
-      // cy.wait(4000)
 
-    cy.get("div").contains(/software development technician/i).should("exist").and("be.visible")
+    // cy.wait(4000)
 
-
-    })
+    cy.get("div")
+      .contains(/software development technician/i)
+      .should("exist")
+      .and("be.visible")
+  })
 })
