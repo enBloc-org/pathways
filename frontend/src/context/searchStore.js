1413b89e2ada72c61ec04bff5cb17ab6a31298a4
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-09-03 15:53:18 +0100

    fix: correct argument declaration
    relates #192

--- frontend/src/context/searchStore.js
+++ frontend/src/context/searchStore.js
@@ -1,4 +1,4 @@
-export default function searchReducer({ state, action }) {
+export default function searchReducer( state, action ) {
   switch (action.type) {
     case "SET_SEARCH_STATUS":
       return { ...state, searchStatus: action.payload }
