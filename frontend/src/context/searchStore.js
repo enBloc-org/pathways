2e8ea0023ba07c3b3f8e145bb8b3528434485fee
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-09-03 14:55:46 +0100

    feat: set up context and reducer
    relates #192

---
+++ frontend/src/context/searchStore.js
@@ -0,0 +1,8 @@
+export default function searchReducer({ state, action }) {
+  switch (action.type) {
+    case "SET_SEARCH_STATUS":
+      return { ...state, searchStatus: action.payload }
+    default:
+      return state
+  }
+}
