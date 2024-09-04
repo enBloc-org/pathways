b095d48eb0ea1885119d5baf8a282f1225497cbe
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-09-04 11:33:31 +0100

    feat: new endpoint for occupationDetails get call
    relates #192

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,50 +1,57 @@
-
 import express from "express"
 import cors from "cors"
 
 import getAllRoutes from "./routes/getAllRoutes.js"
 import getRouteById from "./routes/getRouteById.js"
 import getOccupationByQuery from "./routes/getOccupationByQuery.js"
+import getOccupationDetails from "./routes/getOccupationDetails.js"
 
 const server = express()
 
 const corsOptions = {
-    origin: process.env.FRONTEND,
-    optionsSuccessStatus: 200,
+  origin: process.env.FRONTEND,
+  optionsSuccessStatus: 200,
 }
 
 server.use(cors(corsOptions))
 
 server.get("/getAllRoutes", async (req, res) => {
-    try {
-        const data = await getAllRoutes()
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+  try {
+    const data = await getAllRoutes()
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 server.get("/getRouteById/:id", async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await getRouteById(id)
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+  const { id } = req.params
+  try {
+    const data = await getRouteById(id)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
-server.get(
-    "/getOccupationByQuery/:query",
-    async (req, res) => {
-        const { query } = req.params
-        try {
-            const data = await getOccupationByQuery(query)
-            res.status(200).json(data)
-        } catch (error) {
-            res.status(500).json(error)
-        }
-    }
-)
+server.get("/getOccupationByQuery/:query", async (req, res) => {
+  const { query } = req.params
+  try {
+    const data = await getOccupationByQuery(query)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+server.get("/getOccupationDetails/:stdCode", async (req, res) => {
+  const { stdCode } = req.params
+  try {
+    const data = await getOccupationDetails(stdCode)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 export default server
