656370d2495cdb6aa9ce5d7e95fecbd3e9767fd5
Nich Galzin <nichgalzin@gmail.com>
2024-09-04 15:31:49 +0100

    style: Media queries for large screen sizes
    Relates #193

--- frontend/src/components/TextSearch.jsx
+++ frontend/src/components/TextSearch.jsx
@@ -28,8 +28,7 @@ export default function TextSearch({ searchHandler }) {
         <button className="text-search__button" type="submit">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="1.5em"
-            height="1.5em"
+            className="search-icon"
             viewBox="0 0 256 256"
           >
             <path
