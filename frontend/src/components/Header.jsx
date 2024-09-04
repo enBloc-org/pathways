11842ccd8603b90472a5ca3f2e779b6d04104233
Nich Galzin <nichgalzin@gmail.com>
2024-09-04 14:27:08 +0100

    refactor: updates html to be more semantic and removes uneccessary containers
    Relates #193

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,36 +14,31 @@ export default function Header({ searchHandler }) {
     const match = page.match(pageRegex)
 
     setCurrentPage(match ? match[0] : "/")
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [window.location.href])
 
   return (
-    <nav className="header">
+    <header className="header grid-container">
       <div className="logo-container">
         <Link to="/">
           <img src={gatsbyLogo} alt="Gatsby Logo" className="logo" />
         </Link>
       </div>
-      <div className="header-content">
-        <div>
-          <TextSearch searchHandler={searchHandler} />
-          <p className="information">ℹ</p>
-        </div>
-        <div className="nav-links">
-          <Link
-            className={`header-button ${currentPage === "about" && "bold"}`}
-            to="/about"
-          >
-            About
-          </Link>
-          <Link
-            className={`header-button ${currentPage === "search" && "bold"}`}
-            to="/search"
-          >
-            Search
-          </Link>
-        </div>
-      </div>
-    </nav>
+      <nav className="nav-links">
+        <Link
+          className={`header-button ${currentPage === "about" && "bold"}`}
+          to="/about"
+        >
+          About
+        </Link>
+        <Link
+          className={`header-button ${currentPage === "search" && "bold"}`}
+          to="/search"
+        >
+          Search
+        </Link>
+      </nav>
+      <TextSearch searchHandler={searchHandler} />
+    </header>
   )
 }
