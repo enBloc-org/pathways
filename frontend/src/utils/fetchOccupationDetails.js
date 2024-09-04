d2d447247fe24e68f44b9463fbde06f35e4a67b8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-09-04 12:40:14 +0100

    docs: document new util function
    relates #192

--- frontend/src/utils/fetchOccupationDetails.js
+++ frontend/src/utils/fetchOccupationDetails.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @abstract this util calls the api for the full details of any given occupation to be displayed in `/occupation-details`
+ * @param {string} givenCode expects the stdCode of an occupation present in the api data
+ * @returns the stringified object of the targeted occupation
+ * @throws if our server cannot find the given code of if the code is not a valid string
+ */
 export default async function fetchOccupationDetails(givenCode) {
   try {
     const data = await fetch(
