bd1c268ce89d0b5c369a0561fe81512dbb542713
ShaughnAnderson94 <andersji94@gmail.com>
2024-09-06 15:41:32 +0100

    removed comments and logs

--- frontend/src/components/OccupationCard.jsx
+++ frontend/src/components/OccupationCard.jsx
@@ -13,11 +13,8 @@ const [isMultiLine, setIsMultiLine]= useState(false);
   useEffect(() => {
     const checkMultiLine = () => {
       if (ref.current) {
-        // Create a temporary span element
         const span = document.createElement('span');
-        // Set the text content to the name
         span.textContent = name;
-        // Copy the font styles from the h2 to the span
         const h2Styles = window.getComputedStyle(ref.current);
         span.style.font = h2Styles.font;
         span.style.fontSize = h2Styles.fontSize;
@@ -26,29 +23,24 @@ const [isMultiLine, setIsMultiLine]= useState(false);
         span.style.visibility = 'hidden';
         span.style.position = 'absolute';
         
-        // Add the span to the body
         document.body.appendChild(span);
         
-        // Compare the width of the span to the width of the h2
         const isWrapped = span.offsetWidth > ref.current.offsetWidth;
         
-        // Remove the temporary span
+
         document.body.removeChild(span);
         
         setIsMultiLine(isWrapped);
         
-        console.log('Text width:', span.offsetWidth);
-        console.log('Container width:', ref.current.offsetWidth);
-        console.log('Is multiline:', isWrapped);
+      
       }
     };
 
-console.log('effect ran')
     checkMultiLine();
- // Add event listener for window resize
+
  window.addEventListener('resize', checkMultiLine);
 
- // Cleanup function to remove event listener
+
  return () => window.removeEventListener('resize', checkMultiLine);
   }, [ref]);
   if (!name || !overview || !technicalLevelName) {
