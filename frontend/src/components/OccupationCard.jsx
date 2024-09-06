e94f934f9e2b03c64387fb26423d90a51fc2dc8e
ShaughnAnderson94 <andersji94@gmail.com>
2024-09-06 11:24:41 +0100

    relates #200 first attempt

--- frontend/src/components/OccupationCard.jsx
+++ frontend/src/components/OccupationCard.jsx
@@ -1,4 +1,5 @@
 import {useNavigate} from 'react-router-dom'
+import{useState, useEffect, useRef} from 'react';
 import "../style/OccupationCard.css"
 export default function OccupationCard({
   name,
@@ -6,8 +7,20 @@ export default function OccupationCard({
   technicalLevelName,
   stdCode,
 }) {
+  const ref = useRef(null);
+const [isMultiLine, setIsMultiLine]= useState(false);
   const navigate = useNavigate()
+  useEffect(() => {
+    const checkMultiLine = () => {
+      if (ref.current) {
+        setIsMultiLine(ref.current.scrollHeight > ref.current.clientHeight);
+      }
+    };
 
+    checkMultiLine();
+    // window.addEventListener('resize', checkMultiLine);
+
+  }, [ref, isMultiLine]);
   if (!name || !overview || !technicalLevelName) {
     return <div>Loading...</div>
   }
@@ -17,20 +30,16 @@ export default function OccupationCard({
       className="occupation-card"
       onClick={() => navigate(`/occupation-details/${stdCode}`)}
     >
-      <h2>{name}</h2>
+      <h2 ref={ref} className= {isMultiLine ? 'h2.text-left' :'h2'   } >{name}</h2>
       <div>
         <p className="overview">
           <span>
-            <i>In brief:</i>
+            <i class='in-brief'>In brief:</i>
           </span>{" "}
           {` ${overview}`}
         </p>
       </div>
-      <div className="technical-level-name">
-        <p>
-          <strong>Technical Level:</strong> {technicalLevelName} <br />{" "}
-        </p>
-      </div>
+      
     </button>
   )
 }
